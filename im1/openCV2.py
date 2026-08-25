280aad7198982cf1d2616add5a39d53d571fba9d
MASA <masamostafa2017@gmail.com>
2026-08-25 15:29:27 +0300

    dowloaded open cv and loaded the im0 and im1 in grayscale

--- im1/openCV2.py
+++ im1/openCV2.py
@@ -1,2 +1,9 @@
 import cv2 as cv 
-img = cv.imread()
+img0 = cv.imread('im1/im0.png')
+img1 = cv.imread('im1/im1.png')
+gray0 = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
+gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+cv.imshow('window1', gray0)
+cv.imshow('window2', gray1)
+cv.waitKey(0)
+cv.destroyAllWindows()
